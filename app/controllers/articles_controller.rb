e07c8474c33055795e4d52cf25645863b53c8bac
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-26 15:37:05 -0600

    finish i2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,43 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.destroy(Article.find(params[:id]))
+
+    flash.notice = "Article '#{@article.title}' deleted!"
+
+    redirect_to '/articles'
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
